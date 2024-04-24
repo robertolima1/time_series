a2724bd446c169f9761c7975ef9fced85e7e0fb3
Roberto R <roberto.lima3@outlook.com>
2024-04-24 00:22:53 -0300

    Adiciona criação de curvas sintéticas

--- drift_encoder.R
+++ drift_encoder.R
@@ -9,25 +9,42 @@ source("https://raw.githubusercontent.com/cefet-rj-dal/harbinger/master/R/hcd_ks
 source_python('time_series/seed.py')
 set.seed(1)
 seed_everything(1)
+sample()
 
-n <- 100  # size of each segment
-serie1 <- c(sin((1:n)/pi), 2*sin((1:n)/pi), 10 + sin((1:n)/pi),
-            10-10/n*(1:n)+sin((1:n)/pi)/2, sin((1:n)/pi)/2)
-serie2 <- 2*c(sin((1:n)/pi/6), 2*cos((1:n)/pi), 10 + cos((1:n)/pi /4),
-              10-10/n*(1:n)+cos((1:n)/pi/12)/2, cos((1:n)/pi/6)/2)
-event <- rep(FALSE, length(serie1))
-event[c(100, 200, 300, 400)] <- TRUE
-dataset <- data.frame(serie1, serie2)
+create_random_series <- function(number_elements, range, denominator){
+  series <- c(sample(range, size = number_elements, replace = T))/denominator
+  
+  series
+}
+
+
+# Criar séreies aleatórias
+y = c(create_random_series(100, 5:10, 1), create_random_series(20, 5:10, 1), create_random_series(50, 0:5, 1), create_random_series(50, 0:15, 1))
+x = c(create_random_series(50, 15:20, 1), create_random_series(70, 2:5, 1), create_random_series(25, 5:10, 1), create_random_series(75, 0:4, 1))
+dataset = data.frame("V1" = x, "V2" = y)
+cor(dataset)
+plot_ts(x = 1:length(dataset$V1), y = dataset$V1)
+plot_ts(x = 1:length(dataset$V2), y = dataset$V2)
+ggplot(dataset, aes(x=dataset$V1, y=dataset$V2)) +  geom_point()
+
+#n <- 100  # size of each segment
+#serie1 <- c(sin((1:n)/pi), 2*sin((1:n)/pi), 10 + sin((1:n)/pi),
+#            10-10/n*(1:n)+sin((1:n)/pi)/2, sin((1:n)/pi)/2)
+#serie2 <- 2*c(sin((1:n)/pi/6), 2*cos((1:n)/pi), 10 + cos((1:n)/pi /4),
+#              10-10/n*(1:n)+cos((1:n)/pi/12)/2, cos((1:n)/pi/6)/2)
+#event <- rep(FALSE, length(serie1))
+#event[c(100, 200, 300, 400)] <- TRUE
+#dataset <- data.frame(serie1, serie2)
 
 
 sw_size <- 5
-ts_1<- ts_data(dataset$serie1, sw_size)
-ts_2<- ts_data(dataset$serie2, sw_size)
+ts_1<- ts_data(dataset$V1, sw_size)
+ts_2<- ts_data(dataset$V2, sw_size)
 dataset_ts <- data.frame(ts_1, ts_2)
 
-plot_ts(x = 1:length(dataset$serie1), y = dataset$serie1)
+plot_ts(x = 1:length(dataset$V1), y = dataset$V1)
 
-plot_ts(x = 1:length(dataset$serie2), y = dataset$serie2)
+plot_ts(x = 1:length(dataset$V2), y = dataset$V2)
 
 
 auto <- autoenc_encode(10, 2)
